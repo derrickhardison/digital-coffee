b6bee24823b05ad2837e1a58033793a3fb7dba69
Brian Hestir <brhestir@gmail.com>
2020-12-22 13:55:55 -0500

    Added script.js file and getPexelsImage()

---
+++ script.js
@@ -0,0 +1,56 @@
+///////////////
+// script.js //
+///////////////
+
+// DOM VARIABLES
+
+// JAVASCRIPT VARIABLES
+var intNumImages = 30;      // How many images to get in ajax call to choose from at random
+var strSearchTermArray = ["animals", "people", "cars", "cake"];   // Temporary list of random search terms
+var strSearchTermIndex = Math.floor(Math.random() * strSearchTermArray.length);   // Generate random strsearchTermArray index
+var strSearchTerm = strSearchTermArray[strSearchTermIndex];   // pick one string from array
+
+// FUNCTION DEFINTIONS
+/**
+ *  getPexelsImage(searchTerm, numImages)
+ *  searchTerm: A string specifying the type of images to return
+ *  numImages:  An integer specifying how many images to select from at random
+ */
+function getPexelsImage(searchTerm, numImages){
+  
+  // Validate 0 <= numImages <= 80
+  if (numImages > 80){
+      numImages = 80;
+      throw numImagesTooLargeException;
+  } else if (numImages < 0){
+    numImages = 0;
+    throw numImagesTooSmallException;
+  }
+  
+  // Build queryURL from base API URL and parameters
+  var queryURL = "https://api.pexels.com/v1/search?query=" + searchTerm + "&per_page=" + numImages;
+
+  // Actual API call
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+    headers: {
+      'Authorization': '563492ad6f91700001000001f89041f69d0a47538b315fc967356983'
+    }
+  }).then(function(response){
+    
+    // Choose an image at random from 0 to numImages parameter
+    randomNumber = Math.floor(Math.random() * response.photos.length);
+
+    // May want to parameterize "portrait" to select from an array of available orientations
+    $("body").append($("<img>").attr("src", response.photos[randomNumber].src.portrait));
+  
+  });
+}
+
+// FUNCTION CALLS
+
+// This function appends an element to the body for now due to asynchronous return of .then
+getPexelsImage(strSearchTerm, intNumImages);
+
+// EVENT LISTENERS
